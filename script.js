3921672857cb85e801b23920b569f56ed1c756be
Abdimajid Shire <Abdulmjeedshire3@gmail.com>
2023-09-19 07:12:24 +0300

    added the base styles and css variables and created the navbar

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 "use strict";
+
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  window.scrollY > 100
+    ? navbar.classList.add("navbar--sticky")
+    : navbar.classList.remove("navbar--sticky");
+});
